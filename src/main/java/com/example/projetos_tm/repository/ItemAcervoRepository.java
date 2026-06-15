3f3652771d958688081021c3aa1b38e4e0b4a02c
Germano Spall Figueira <gspallfigueira@gmail.com>
2026-06-15 09:17:43 -0300

    alteracoes feitas em aula

--- src/main/java/com/example/projetos_tm/repository/ItemAcervoRepository.java
+++ src/main/java/com/example/projetos_tm/repository/ItemAcervoRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface ItemAcervoRepository extends JpaRepository<ItemAcervo, Long> {
+public interface ItemAcervoRepository extends JpaRepository<ItemAcervo, UUID> {
     List<ItemAcervo> findByNome(String nome);
     List<ItemAcervo> findByTipo(String tipo);
     List<ItemAcervo> findByDescricao(String descricao);
